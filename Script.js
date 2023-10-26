64d9c26d803a8183dfda362bacb7cb4c4d0121e6
clementomondi <clementomondi2004@gmail.com>
2023-10-26 17:10:05 +0300

    dropdown

---
+++ Script.js
@@ -0,0 +1,7 @@
+// Add JavaScript code here
+const card = document.querySelector('.card');
+const cardtoggle = document.querySelector('.toggle');
+
+cardtoggle.onclick = function () {
+    card.classList.toggle('active')
+}
